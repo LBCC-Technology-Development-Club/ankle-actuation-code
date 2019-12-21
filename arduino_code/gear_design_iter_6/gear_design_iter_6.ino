6b620dbc3102021619dcf0a840ceceb802d44fb7
SrikarValluri <srikar.valluri.7060@mail.linnbenton.edu>
2019-12-21 14:44:41 -0800

    Started gear_design_iter_6

---
+++ arduino_code/gear_design_iter_6/gear_design_iter_6.ino
@@ -0,0 +1,23 @@
+#include <motor.h>
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
+
+void motor_clockwise(struct motor motor) {
+
+
+}
+
+void motor_counterclockwise(struct motor motor) {
+
+
+
+}
+
---
+++ arduino_code/gear_design_iter_6/motor.h
@@ -0,0 +1,9 @@
+struct motor {
+  // Ports
+  int EN;
+  int IN1;
+  int IN2;
+  
+  //Speed
+  float speed;
+};
